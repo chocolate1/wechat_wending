eaa0cb0681d2c38f35da29fc2bf941bdc6b7eb43
chocolate1 <201532695@qq.com>
2017-08-08 19:49:19 +0800

    'upload-gif'

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,20 +1,4 @@
-// var app = getApp()
-// Page({
-//   data: {
-//     userInfo: {}
-//   },
-//   onLoad: function () {
-//     console.log('onLoad')
-//     var that = this
-//     //调用应用实例的方法获取全局数据
-//     app.getUserInfo(function(userInfo){
-//       //更新数据
-//       that.setData({
-//         userInfo:userInfo
-//       })
-//     })
-//   }
-// })
+
 Page({
   data: {
    tempFilePaths: '',
@@ -83,6 +67,7 @@ Page({
       complete: function() {
         // complete
         console.log("获取用户信息完成！")
+        console.log(this.province)
       }
     })
   }
